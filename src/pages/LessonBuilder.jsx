956a19cf662f23b41d6e9f3d406971a86ca6ee88
daniyalh11 <hashmi@creditoracademy.com>
2025-09-24 14:51:24 +0530

    updated

--- src/pages/LessonBuilder.jsx
+++ src/pages/LessonBuilder.jsx
@@ -5506,7 +5506,7 @@ function LessonBuilder() {
                 <Button
                   variant="outline"
                   size="sm"
-                  onClick={handlePreview}
+                 // onClick={handlePreview}
                   className="flex items-center gap-1"
                 >
                   <Eye className="h-4 w-4 mr-1" />
