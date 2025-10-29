a644bf4d3e0e1a35d6ccebdac5131b8a41ed5b1b
PrernaCreditor <prerna@creditoracademy.com>
2025-10-29 14:09:17 +0530

    update in platform section

--- src/pages/Platform/MembershipsPage.jsx
+++ src/pages/Platform/MembershipsPage.jsx
@@ -1,14 +1,23 @@
 import React from 'react';
 import Navbar from '../../components/navbar';
 import Footer from '../../components/Footer';
-import Membership from '../../components/Platform/Membership/Membership';
-
+import Membership from '../../components/Platform/Membership/Membershiphero';
+import Membershipincome from '../../components/Platform/Membership/Membershipincome';
+import Membershipcontent from '../../components/Platform/Membership/Membershipcontent';
+import Membershipready from '../../components/Platform/Membership/Membershipready';
+import Membershipfaq from '../../components/Platform/Membership/Membershipfaq';
+import Membershipscale from '../../components/Platform/Membership/Membershipscale';
 const MembershipsPage = () => {
   return (
     <div>
       <Navbar />
       <Membership />
-      {/* Add more membership-related components here */}
+      <Membershipincome />
+      <Membershipcontent />
+      <Membershipready />
+      <Membershipfaq />
+      <Membershipscale />
+        {/* Add more membership-related components here */}
       <Footer />
     </div>
   );
