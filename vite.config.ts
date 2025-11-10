e5858ea8d37e4ea95320f25aead3e97ce695e0b4
singh-gitignore <singh@creditoracademy.com>
2025-11-10 10:45:27 +0530

    env

--- vite.config.ts
+++ vite.config.ts
@@ -80,7 +80,7 @@ export default defineConfig(({ mode }) => ({
   },
   define: {
     'import.meta.env.VITE_API_BASE_URL': JSON.stringify(
-      'https://creditor.onrender.com'
+      'https://testbackend-hcoy.onrender.com'
     ),
   },
 }));
