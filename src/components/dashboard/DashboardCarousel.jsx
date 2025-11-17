0d7e05c3a17b763a5e99f77a3a4b7c6944fba63f
Ashutosh Kandpal <ashutosh@creditoracademy.com>
2025-11-17 16:23:22 +0530

    Banners Updated

--- src/components/dashboard/DashboardCarousel.jsx
+++ src/components/dashboard/DashboardCarousel.jsx
@@ -18,11 +18,35 @@ const carouselItems = [
   // },
   {
     id: 1,
-    type: "image",
-    image: "https://lesson-banners.s3.us-east-1.amazonaws.com/Upcoming_Courses_Banner/22nov.png",
-    title: "This Saturday",
-    course: "8th Nov",
-    
+    type: 'image',
+    image:
+      'https://lesson-banners.s3.us-east-1.amazonaws.com/Upcoming_Courses_Banner/22nov.png',
+    title: 'This Saturday',
+    course: '8th Nov',
+  },
+  {
+    id: 2,
+    type: 'image',
+    image:
+      'https://lesson-banners.s3.us-east-1.amazonaws.com/Upcoming_Courses_Banner/22novpart2.png',
+    title: 'Upcoming Event',
+    course: 'Banner 2',
+  },
+  {
+    id: 3,
+    type: 'image',
+    image:
+      'https://lesson-banners.s3.us-east-1.amazonaws.com/Upcoming_Courses_Banner/nonmember.png',
+    title: 'Upcoming Event',
+    course: 'Banner 3',
+  },
+  {
+    id: 3,
+    type: 'image',
+    image:
+      'https://lesson-banners.s3.us-east-1.amazonaws.com/Upcoming_Courses_Banner/Thanksgiving.png',
+    title: 'Upcoming Event',
+    course: 'Banner 3',
   },
 ];
 
