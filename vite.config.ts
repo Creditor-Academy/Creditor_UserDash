c0c25c87ea72f97495cde6fcba6c09812c73ffce
ashutosh <ashutosh@creditoracademy.com>
2025-11-26 17:06:58 +0530

    Env changes

--- vite.config.ts
+++ vite.config.ts
@@ -81,7 +81,7 @@ export default defineConfig(({ mode }) => ({
   },
   define: {
     'import.meta.env.VITE_API_BASE_URL': JSON.stringify(
-      'https://creditor-backend-lfre.onrender.com'
+      'https://testbackend-hcoy.onrender.com'
     ),
   },
 }));
