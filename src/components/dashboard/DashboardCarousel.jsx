30dff7b2cbcb5f1a530e513b83b6c334ad3a1dc3
Ashutosh <ashutosh.kandpal@lmsathena.com>
2025-12-30 02:54:07 -0800

    Banners Updated

--- src/components/dashboard/DashboardCarousel.jsx
+++ src/components/dashboard/DashboardCarousel.jsx
@@ -29,7 +29,7 @@ const carouselItems = [
     id: 2,
     type: 'image',
     image:
-      'https://lesson-banners.s3.us-east-1.amazonaws.com/Dashboard-banners/1.png',
+      'https://athena-user-assets.s3.eu-north-1.amazonaws.com/Upcoming_events_Banner/1+(1).png',
     title: 'Upcoming Event',
     course: 'Banner 2',
   },
@@ -37,7 +37,7 @@ const carouselItems = [
     id: 3,
     type: 'image',
     image:
-      'https://lesson-banners.s3.us-east-1.amazonaws.com/Dashboard-banners/2.png',
+      'https://athena-user-assets.s3.eu-north-1.amazonaws.com/Upcoming_events_Banner/2+(1).png',
     title: 'Upcoming Event',
     course: 'Banner 3',
   },
